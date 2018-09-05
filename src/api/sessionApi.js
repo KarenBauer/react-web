3b0a9f9334488371115f23b52b6a3b85f05b3d5a
Karen <karenabuerpej@gmail.com>
2018-09-04 23:04:01 -0300

    Initial commit

--- src/api/sessionApi.js
+++ src/api/sessionApi.js
@@ -2,15 +2,15 @@ import api from './apiService';
 
 class Session {
   static login(user) {
-    return api.post('/users/sign_in', user);
+    return api.post('/offerers/sign_in', user);
   }
 
   static logout() {
-    return api.delete('/users/sign_out');
+    return api.delete('/offerers/sign_out');
   }
 
   static signUp(user) {
-    return api.post('/users', user);
+    return api.post('/offerers', user);
   }
 }
 
